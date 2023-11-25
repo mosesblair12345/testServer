461d817c266e15731a5a43e9285cb783f98ddb67
moses <mosessblair00@gmail.com>
2023-11-25 12:04:27 +0300

    server v2

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = 6000;
 
 const validateMsisdn = async (msisdn) => {
